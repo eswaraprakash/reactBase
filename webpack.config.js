15aa854cc10536080d01f7cf300d70e33af557f5
EswaraPrakash <evaithiy@cisco.com>
2017-11-03 13:14:57 +0800

    Base Prod config commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,29 @@
 //webpack.config.js
-
+var webpack = require('webpack');
 module.exports = {
     entry: ['./app/main.js'],
     output: {
+        path: __dirname,
         filename: 'bundle.js'
     },
     module: {
-        loaders: [
-            {
+        loaders: [{
                 loader: 'babel-loader',
                 test: /\.js$/,
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
             }
         ]
     },
     devServer: {
         port: 3000
     },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    ]
 };
