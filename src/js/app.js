0a477625f7dca8b1734503db2e9cf5614deddeb2
EswaraPrakash <evaithiy@cisco.com>
2017-11-03 14:51:10 +0800

    React base with babel and webpack configs

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+//App.js
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import '../css/style.css';
+
+import jsImage from '../assets/js.png';
+
+export default class Hello extends Component {
+    render(){
+        return (
+            <div>
+                React Base framework
+                <img src={jsImage} alt='CommanderJS' />
+            </div>
+        );
+    }
+}
+render(<Hello />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,70 @@
 //webpack.config.js
-var webpack = require('webpack');
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// Constant with our paths
+const paths = {
+    DIST: path.resolve(__dirname, 'dist'),
+    SRC: path.resolve(__dirname, 'src'),
+    JS: path.resolve(__dirname, 'src/js'),
+};
 module.exports = {
-    entry: ['./app/main.js'],
+    entry: path.join(paths.JS, 'app.js'),
     output: {
-        path: __dirname,
-        filename: 'bundle.js'
+        path: paths.DIST,
+        filename: 'app.bundle.js'
     },
+    // Loaders configuration
+    // We are telling webpack to use "babel-loader" for .js and .jsx files
     module: {
-        loaders: [{
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                ],
             },
+            // CSS loader to CSS files -> ADDED IN THIS STEP
+            // Files will get handled by css loader and then passed to the extract text plugin
+            // which will write it to the file we defined above
             {
                 test: /\.css$/,
-                loader: "style!css"
-            }
-        ]
+                loader: ExtractTextPlugin.extract({
+                    use: 'css-loader',
+                }),
+            },
+            // File loader for image assets -> ADDED IN THIS STEP
+            // We'll add only image extensions, but you can things like svgs, fonts and videos
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+    // Enable importing JS files without specifying their's extenstion
+    //
+    // So we can write:
+    // import MyComponent from './my-component';
+    //
+    // Instead of:
+    // import MyComponent from './my-component.jsx';
+    resolve: {
+        extensions: ['.js', '.jsx'],
     },
     devServer: {
-        port: 3000
+        port: 3000,
+        contentBase: paths.SRC,
     },
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
-        })
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(paths.SRC, 'index.html'),
+        }),
+        new ExtractTextPlugin('style.bundle.css'),
     ]
 };
