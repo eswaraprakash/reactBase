a6d8157dd1eda3a69bbbbe67167e3f73f55f0d8b
EswaraPrakash <evaithiy@cisco.com>
2017-11-03 16:53:33 +0800

    mocha test framework

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,6 @@ import { render } from 'react-dom';
 import '../css/style.less';
 
 import jsImage from '../assets/js.png';
-import tableImage from '../assets/iot-table.jpeg';
 
 export default class Hello extends Component {
     render(){
@@ -13,7 +12,6 @@ export default class Hello extends Component {
                 React Base framework
                 <div>
                     <img src={jsImage} alt='CommanderJS' />
-                    <img src={tableImage} />
                 </div>
             </div>
         );
